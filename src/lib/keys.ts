29a72e11abe63f5088bd7e3e86cdf4549c54b1b7
Ben Durrant <ben.j.durrant@gmail.com>
2025-03-01 22:36:39 +0000

    noSuggest -> hide

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -1,7 +1,7 @@
-const noSuggest = <T extends string>(key: T) => ` ${key}` as const;
+const hide = <T extends string>(key: T) => ` ${key}` as const;
 const dissuade = <T extends string>(key: T) => `~${key}` as const;
 
-export const types = noSuggest("types");
+export const types = hide("types");
 export const type = dissuade("type");
 export const name = dissuade("name");
 export const variant = dissuade("variant");
