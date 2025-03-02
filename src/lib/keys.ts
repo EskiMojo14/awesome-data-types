d77040383f8609c9390d954f33ecb60e782d0852
Ben Durrant <ben.j.durrant@gmail.com>
2025-03-02 11:15:08 +0000

    mark key creation as pure

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -1,7 +1,7 @@
 const hide = <T extends string>(key: T) => ` ${key}` as const;
 const dissuade = <T extends string>(key: T) => `~${key}` as const;
 
-export const types = hide("types");
-export const type = dissuade("type");
-export const name = dissuade("name");
-export const variant = dissuade("variant");
+export const types = /*#__PURE__*/ hide("types");
+export const type = /*#__PURE__*/ dissuade("type");
+export const name = /*#__PURE__*/ dissuade("name");
+export const variant = /*#__PURE__*/ dissuade("variant");
