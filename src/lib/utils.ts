55cf556cdd2ef685bcbf2c5229ff51f56101c98b
Ben Durrant <ben.j.durrant@gmail.com>
2025-03-02 11:16:17 +0000

    formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,6 @@ export type LooseAutocomplete<T> = T | NonReducibleUnknown;
 export type NoInfer<T> = [T][T extends any ? 0 : never];
 export type Override<T, U> = Omit<T, keyof U> & U;
 export type AnyFn = (...args: any) => any;
-
 export type UnionHasOneMember<T, TCopy = T> = [T] extends [never]
   ? false
   : T extends unknown
@@ -12,13 +11,12 @@ export type UnionHasOneMember<T, TCopy = T> = [T] extends [never]
       ? true
       : false
     : never;
+
 export function assert(
   condition: unknown,
   message?: string,
 ): asserts condition {
-  if (!condition) {
-    throw new Error(message);
-  }
+  if (!condition) throw new Error(message);
 }
 
 export const objectKeys = Object.keys as <Obj extends object>(
