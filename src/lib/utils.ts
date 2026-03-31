d260d9094691c8428dcdef8918da90e1f1c7ad83
Ben Durrant <ben.durrant@marketdojo.com>
2026-03-31 15:34:08 +0100

    add knip and remove unused deps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 export type NotNullish = NonNullable<unknown>;
 export type NonReducibleUnknown = NotNullish | null | undefined;
 export type LooseAutocomplete<T> = T | NonReducibleUnknown;
-export type NoInfer<T> = [T][T extends any ? 0 : never];
 export type Override<T, U> = Omit<T, keyof U> & U;
 export type AnyFn = (...args: any) => any;
 export type UnionHasOneMember<T, TCopy = T> = [T] extends [never]
