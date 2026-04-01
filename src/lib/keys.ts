6e3dc9537c5e9253f7966d9e071ed4c1c0bf0007
Ben Durrant <ben.durrant@marketdojo.com>
2026-04-01 11:53:43 +0100

    simplify keys

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -1,6 +1,4 @@
-const dissuade = /* #__NO_SIDE_EFFECTS__ */ <T extends string>(key: T) => `~${key}` as const;
-
-export const type: "~type" = dissuade("type");
-export const name: "~name" = dissuade("name");
-export const variant: "~variant" = dissuade("variant");
-export const variants: "~variants" = dissuade("variants");
+export const type = "~type";
+export const name = "~name";
+export const variant = "~variant";
+export const variants = "~variants";
